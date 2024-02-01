99fe1971e69e2e7a364e06f6e26900e020e6a7bd
yuta0403 <kanbenoyuta@gmail.com>
2024-02-01 23:55:34 +0900

    フィードのUIを作成

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -1,12 +1,124 @@
 import 'package:flutter/material.dart';
 
 class FeedPage extends StatelessWidget {
-  const FeedPage({Key? key}) : super(key: key);
+  FeedPage({Key? key}) : super(key: key);
+  final images = [
+    'https://pixabay.com/get/g28288d7ec2f679a835a995e67fe2343153a6f65f97914f30c21bdb8a30a65fe0a3cee4604fba4de9dee4a21ae5e5c012_640.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('フィード')),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Image.network(
+                    'https://appliv-domestic.akamaized.net/v1/600x/r/articles/129815/13877626_1604328764_041813000_0_1500_1500.jpeg',
+                    width: 40,
+                    height: 40,
+                  ),
+                  const SizedBox(width: 8),
+                  const Column(
+                    children: [
+                      Text(
+                        'instagram',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text('サンディエゴ'),
+                    ],
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.more_horiz,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            GridView.count(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              crossAxisCount: 1,
+              children: images.map((imageUrl) {
+                return InstagramPostItem(imageUrl: imageUrl);
+              }).toList(),
+            ),
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.favorite_border,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.message,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.send,
+                  ),
+                  onPressed: () {},
+                ),
+                const Spacer(),
+                IconButton(
+                  icon: const Icon(
+                    Icons.bookmark_border_outlined,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Text(
+                '「いいね！」100,000件',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                'Flutter, Google UI toolkit, enables efficient cross-platform app development with its rapid hot reload, versatile widget architecture, and seamless integration across mobile, web, and desktop. Supported by Dart, Flutter offers an enjoyable coding experience and visually appealing designs, backed by a vibrant community and major platform support.',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InstagramPostItem extends StatelessWidget {
+  InstagramPostItem({Key? key, required this.imageUrl}) : super(key: key);
+  final String imageUrl;
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
     );
   }
 }
